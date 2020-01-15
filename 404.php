385dd7a5f7f45f2afbdafb36786094b276af0955
Doug La Farge <doug@lafarge.net>
2020-01-15 10:38:18 -0700

    Created New Theme Repositor

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found). This i a change.
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package dlafargesimple
+ */
+
+get_header(); ?>
+
+<?php get_template_part( 'template-parts/content', 'none' ); ?>
